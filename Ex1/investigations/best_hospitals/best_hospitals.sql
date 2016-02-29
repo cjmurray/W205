147e421faecd337a0cb6cf2a76d7a6889fc37a19
cjmurray <w205@ip-172-31-59-210.ec2.internal>
2016-02-29 04:23:52 +0000

    Changed to use a map-reduce SQL query.

--- Ex1/investigations/best_hospitals/best_hospitals.sql
+++ Ex1/investigations/best_hospitals/best_hospitals.sql
@@ -1,1 +1,6 @@
-SELECT HCAHPSBaseScore, HCAHPSConsistencyScore, ProviderNumber, HospName FROM surveys_responses_x ORDER BY HCAHPSBaseScore DESC, HCAHPSConsistencyScore DESC LIMIT 10;
+SELECT DISTINCT AVG(Score) OVER (PARTITION BY ProviderID) AS avgScore, ProviderID, HospitalName 
+FROM effective_care_x 
+WHERE Score > 0 AND Score <= 100 
+GROUP BY ProviderID, HospitalName, Score 
+ORDER BY avgScore DESC 
+LIMIT 10;
