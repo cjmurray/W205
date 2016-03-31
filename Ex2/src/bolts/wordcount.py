ed31acc6114511512fcdd36585d31b7159619dea
root <root@ip-172-31-51-162.ec2.internal>
2016-03-31 17:25:40 +0000

    Completed Step 4.2 - finalresults.py

--- Ex2/src/bolts/wordcount.py
+++ Ex2/src/bolts/wordcount.py
@@ -12,15 +12,15 @@ class WordCounter(Bolt):
         self.counts = Counter()
 
 	# connect to database and create table	
-	# conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", host="localhost", port="5432")
+	#conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", host="localhost", port="5432")
 
-	# The old version of Postgres we are using does not support IF NOT EXISTS!!
-	# so create the table manually with psql...
-
+	# Why does this throw an error???
 	#cur = conn.cursor()
-	#cur.execute('''CREATE TABLE IF NOT EXISTS Tweetwordcount (word TEXT PRIMARY KEY NOT NULL, count INT NOT NULL);''')
+	#cur.execute('''DROP TABLE IF EXISTS Tweetwordcount;''')
 	#conn.commit()
-	# conn.close()
+	#cur.execute('''CREATE TABLE Tweetwordcount (word TEXT PRIMARY KEY NOT NULL, count INT NOT NULL);''')
+	#conn.commit()
+	#conn.close()
 
 
     def process(self, tup):
@@ -28,25 +28,18 @@ class WordCounter(Bolt):
 
 	conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", host="localhost", port="5432")
 
-        # Increment the local count
-        self.counts[word] += 1
-        self.emit([word, self.counts[word]])
-
-        # Write codes to increment the word count in Postgres
-        # Use psycopg to interact with Postgres
-        # Database name: Tcount 
-        # Table name: Tweetwordcount 
-        # you need to create both the database and the table in advance.
 	cur = conn.cursor()
-	# cur.execute("INSERT INTO Tweetwordcount (word,count) VALUES ('%s', 1)" % word);
 	cur.execute("INSERT INTO Tweetwordcount (word,count) SELECT '%s', 0 WHERE NOT EXISTS (SELECT * FROM Tweetwordcount WHERE word='%s')" % (word, word))
 	conn.commit()
 
-	# cur.execute("UPDATE Tweetwordcount SET count=%s WHERE word='%s'" % (self.counts[word], word))
 	cur.execute("UPDATE Tweetwordcount SET count = count + 1 WHERE word='%s'" % word)
 	conn.commit()
 	
 	conn.close()
+
+        # Increment the local count
+        self.counts[word] += 1
+        self.emit([word, self.counts[word]])
 
         # Log the count - just to see the topology running
         self.log('%s: %d' % (word, self.counts[word]))
